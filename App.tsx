f1c4922a8061e41f06eac53d849c59ee2ccb3aa9
LucianoVelasquez <125805986+LucianoVelasquez@users.noreply.github.com>
2024-06-04 17:38:49 -0300

    first

--- App.tsx
+++ App.tsx
@@ -1,13 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View,Text } from 'react-native';
+import {  Image,Input,Button } from '@rneui/themed';
+import { Mail,KeyRound   } from 'lucide-react-native';
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>hELLO</Text>
-      <Text>Hola</Text>
+      <View style={{flex:1, overflow:'hidden'}}>
+        <Image source={{uri:"https://raw.githubusercontent.com/LucianoVelasquez/front-app-agua/main/assets/undraw_QA_engineers_dg5p.png"}} style={{width:380,height:300,objectFit:"contain"}}></Image>
+      </View>
+      
+      <View style={{flex:2,width:"100%",alignItems:'center',marginTop:50}}>
+        <Text style={{fontSize:36,fontWeight:'500'}}>Inicia Session</Text>
+
+        <View style={{width:"75%",marginTop:50}}>
+          <Input
+          placeholder='  Email'
+          style={{}}
+          leftIcon={
+          <Mail
+            size={24}
+            color='black'
+          />
+          }
+          />
+          <Input
+          placeholder='  Password'
+          style={{}}
+          leftIcon={
+          <KeyRound
+            size={24}
+            color='black'
+          />
+          }
+          />   
+          <Button
+                title="LOG IN"
+                buttonStyle={{
+                  backgroundColor: 'black',
+                  borderWidth: 2,
+                  borderColor: 'white',
+                  borderRadius: 30,
+                }}
+                containerStyle={{
+                  width: 270,
+                  marginHorizontal: 10,
+                  marginVertical: 10,
+                }}
+                titleStyle={{ fontWeight: 'bold' }}
+              />
+        </View>
+        
+      </View>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
